52b27e7b549fc3fb130aa51d331d2c152c747e76
Lê Đức Thuận <87764627+YuukiCute@users.noreply.github.com>
2023-03-09 20:42:12 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: Yuu
+ */
+$("#login-button").click(function (event) {
+    var userName = document.getElementById("userName").value;
+    var pwd = document.getElementById("pwd").value;
+    if (userName == "ThuHuynXinhDep" && pwd == "0903") {
+      event.preventDefault();
+      $("form").fadeOut(500);
+      $(".wrapper").addClass("form-success");
+      setTimeout(function () {
+        location.href = "BirthdayCake.html";
+      }, 2000);
+    } else {
+      alert("Wrong Password");
+    }
+  });
